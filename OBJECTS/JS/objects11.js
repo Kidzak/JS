6106d9cbb4ff8cd507dc0dde20ef172d5c270083
Kidzak <mikolajkijak549@gmail.com>
2023-11-27 12:08:55 +0100

    modified:   OBJECTS/JS/objects11.js

--- OBJECTS/JS/objects11.js
+++ OBJECTS/JS/objects11.js
@@ -8,8 +8,8 @@ function stworzPana(name, address, bodydimensions) {
     };
 }
 
-const user1 = stworzPana("usuario1", "choco street", {height: 183, weight: 77});
-const user2 = stworzPana("usuario2", "coconut street", {height: 153, weight: 46});
+const user1 = stworzPana("usuario1", "choco street", "height: 183, weight: 77");
+const user2 = stworzPana("usuario2", "coconut street", "height: 153, weight: 46");
 
 console.log(user1)
 console.log(user2)
